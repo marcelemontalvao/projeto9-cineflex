553b072fde386ec01eee15b1f17874563db62b7a
marcele <mmontalvaoblog@outlook.com>
2022-05-18 17:56:11 -0300

    Layout com as imagens

--- src/SelecionarAssento.js
+++ src/SelecionarAssento.js
@@ -1,6 +1,7 @@
 export default function SelecionarAssento () {
     return (
         <div>
+            aaaaaaaaaaaaaaaaaaaa
         </div>
     )
 }
--- src/SelecionarFilme.js
+++ src/SelecionarFilme.js
@@ -1,23 +1,25 @@
 import axios from "axios"
-import React from "react"
 import {useState, useEffect} from "react"
  
 export default function SelecionarFilme () {
-    //const [image, setImage] = useState({})
+    const [image, setImage] = useState([])
     
-   // useEffect(() => {
-    //axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies").then(response => {setImage(...response.data.posterURL) }
-    //    ).catch("carregando")
-   // }, [])
+    useEffect(() => {  const request = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies")
+    
+    request.then(response => {setImage(response.data) }
+  ).catch("carregando")
+   }, [])
     return (
         <>
             <header>
                 <p>CINEFLEX</p>
             </header>
-            
-            <p>Selecione o Filme</p>
-            <div>
-                <img></img>
+            <div className="container">
+                <p>Selecione o Filme</p>
+                {image.map( (images, index) =>  <div key={index} className="image">
+                    <img src={images.posterURL} alt="filmes"  /> 
+                </div> )}
+               
             </div>
         </>
     )
